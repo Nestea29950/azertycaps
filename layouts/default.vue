5169560bf2837aca2d78fd4cccee2a5d855e7808
William Gaonarc'h <72440100+Nestea29950@users.noreply.github.com>
2025-04-10 11:00:03 +0200

    z

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 import { Analytics } from '@vercel/analytics/nuxt'
+
+if (process.server) {
+  console.log('SSR - Côté serveur')
+} else {
+  console.log('SSG / CSR - Côté client')
+}
 </script>
 
 <template>
