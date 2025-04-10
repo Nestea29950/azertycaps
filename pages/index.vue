aeeccb95b8c72c33e57bce5365789cb02e38056b
William Gaonarc'h <72440100+Nestea29950@users.noreply.github.com>
2025-04-10 11:20:46 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,23 @@ useHead({
   meta: [
     { name: 'description', content: 'Découvrez les meilleurs keycaps Azerty pour personnaliser votre clavier mécanique français ISO. Explorez notre sélection de produits de haute qualité.' },
     { name: 'keywords', content: 'keycaps Azerty, acheter keycaps, accessoires clavier, claviers mécaniques, keycaps pour clavier, keycaps personnalisés, clavier français ISO' },
-    { name: 'robots', content: 'index, follow' },  // permet aux moteurs de recherche d'indexer la page
+    { name: 'robots', content: 'index, follow' },
     { name: 'author', content: 'azertycaps.fr' },
     { name: 'og:title', content: 'Keycaps Azerty - Personnalisez votre Clavier Mécanique Français ISO' },
     { name: 'og:description', content: 'Découvrez une sélection de keycaps Azerty pour améliorer votre expérience clavier. Trouvez les meilleurs produits pour votre clavier mécanique français ISO.' },
-    { name: 'og:image', content: '/path/to/your/og-image.jpg' }, // Remplace cette URL par celle de ton image d'aperçu
+    { name: 'og:image', content: '/path/to/your/og-image.jpg' },
     { name: 'og:url', content: 'https://azertycaps.fr' },
     { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:title', content: 'Keycaps Azerty - Achetez des Keycaps pour Clavier Mécanique Français ISO' },
     { name: 'twitter:description', content: 'Trouvez des keycaps Azerty de haute qualité pour personnaliser votre clavier mécanique français ISO.' },
-    { name: 'twitter:image', content: '/path/to/your/og-image.jpg' } // Remplace cette URL par celle de ton image d'aperçu
-  ],
+    { name: 'twitter:image', content: '/path/to/your/og-image.jpg' }
+  ]
 });
 
-
 const supabase = useSupabaseClient();
-const ListClavierData = ref([]);
 const errorMessage = ref('');
 
-onMounted(async () => {
+const { data, error } = await useAsyncData('claviersData', async () => {
   try {
     const { data, error } = await supabase
       .from('claviers')
@@ -38,17 +36,17 @@ onMounted(async () => {
       throw new Error(error.message);
     }
 
-    ListClavierData.value = data;
+    return data;
   } catch (error) {
     errorMessage.value = error.message;
+    return [];  // Retourner un tableau vide si une erreur se produit
   }
 });
-
 </script>
 
 <template>
   <div>
-    
-    <app-list-product :ListClavier="ListClavierData" />
+    <!-- Passe 'data' directement à la prop 'ListClavier' -->
+    <app-list-product :ListClavier="data" />
   </div>
 </template>
