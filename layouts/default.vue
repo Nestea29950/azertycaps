d63917f7fdc0c9306ff07f0b723db4a5f37258fc
William Gaonarc'h <72440100+Nestea29950@users.noreply.github.com>
2025-04-10 10:35:25 +0200

    z

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
+<script setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
 <template>
     <div>
+      <SpeedInsights/>
       <AppHeader />
       <slot />
       <AppFooter />
